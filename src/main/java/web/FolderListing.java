28dd30df056d1795920832562f9b5fd4bc533565
Mikhail Arkhipov <miklpostbox@gmail.com>
2019-11-28 17:33:08 +0300

    Corrected remarks

--- src/main/java/web/FolderListing.java
+++ src/main/java/web/FolderListing.java
@@ -8,7 +8,7 @@ public class FolderListing implements IFolderListing {
 
     @Override
     public String getListing(String folderName) {
-        File file = new File(folderName);
+        final File file = new File(folderName);
         if(file.isDirectory()){
             return folderName + "\\<br>"+  getContentOfFolder(file.getAbsoluteFile(),0).toString();
         }
@@ -19,13 +19,11 @@ public class FolderListing implements IFolderListing {
 
     private static StringBuffer getContentOfFolder(File currentFile, int level) {
         level++;
-        StringBuffer listOfFiles = new StringBuffer();
+        final StringBuffer listOfFiles = new StringBuffer();
         if (currentFile != null) {
             File[] files = currentFile.getAbsoluteFile().listFiles();
             for (File file : files) {
-                for (int i = 0; i < level*4 ; i++) {
-                    listOfFiles.append("&nbsp;");
-                }
+                listOfFiles.append(addWhiteSpaces(level));
                 listOfFiles.append(file.getName());
                 if (file.isDirectory()) {
                     listOfFiles.append("\\");
@@ -37,5 +35,13 @@ public class FolderListing implements IFolderListing {
             }
         }
         return listOfFiles;
+    }
+
+    private static String addWhiteSpaces(int level) {
+        final StringBuilder whiteSpace = new StringBuilder(new String());
+        for (int i = 0; i < level*4 ; i++) {
+            whiteSpace.append("&nbsp;");
+        }
+        return whiteSpace.toString();
     }
 }
